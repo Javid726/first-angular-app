aa1a9b66aba3a83a1b7c18da92b01ad2c4597f18
Javid Gasimov <javidgasimov98@gmail.com>
2025-01-30 15:26:41 +0400

    added form submission

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,30 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { type NewTask } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() close = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTask>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onCloseDialog() {
     this.close.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      dueDate: this.enteredDate,
+    });
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -3,6 +3,7 @@ import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 
 import { dummyTasks } from '../dummy-tasks';
+import { type NewTask } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -28,6 +29,18 @@ export class TasksComponent {
     this.isAddingTask = true;
   }
   onCloseDialog() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTask) {
+    this.tasks.push({
+      id: new Date().getMilliseconds().toString(),
+      userId: this.id,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.dueDate,
+    });
+
     this.isAddingTask = false;
   }
 }
